90a245bd4f0d4772d01edd4aac1ab6872482ca8b
agh34 <aaronhuang2024@u.northwestern.edu>
2023-04-05 23:05:00 -0500

    HW3

---
+++ HW3/hw3.c
@@ -0,0 +1,43 @@
+#include "nu32dip.h" // constants, functions for startup and UART
+
+void blink(int, int); // blink the LEDs function
+
+int main(void) {
+  char message[100];
+  NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
+  NU32DIP_WriteUART1("Hello\r\n");
+  while (1) {
+      while(!NU32DIP_USER){
+      int i = 0;
+      for(i = 0; i < 100; i++){
+          sprintf(message,"%f\r\n",i*3.3/100.0);
+          NU32DIP_WriteUART1(message); // send message back
+          _CP0_SET_COUNT(0);
+          while(_CP0_GET_COUNT()<24000){}
+
+      }
+      }
+
+}
+}
+
+// blink the LEDs
+void blink(int iterations, int time_ms){
+	int i;
+	unsigned int t;
+	for (i=0; i< iterations; i++){
+		NU32DIP_GREEN = 0; // on
+		NU32DIP_YELLOW = 1; // off
+		t = _CP0_GET_COUNT(); // should really check for overflow here
+		// the core timer ticks at half the SYSCLK, so 24000000 times per second
+		// so each millisecond is 24000 ticks
+		// wait half in each delay
+		while(_CP0_GET_COUNT() < t + 12000*time_ms){}
+		
+		NU32DIP_GREEN = 1; // off
+		NU32DIP_YELLOW = 0; // on
+		t = _CP0_GET_COUNT(); // should really check for overflow here
+		while(_CP0_GET_COUNT() < t + 12000*time_ms){}
+	}
+}
+		
